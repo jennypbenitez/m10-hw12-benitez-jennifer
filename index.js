381af60be558269172243e5cf2ec50befd0d793b
My Name <jenniferbenitez@ufl.edu>
2021-08-04 18:48:06 -0400

    uploading files

---
+++ index.js
@@ -0,0 +1,21 @@
+class Car {
+    constructor(make, model, year) {
+      this.make = make
+      this.model = model
+      this.year = year
+    }
+    honk() {
+      console.log("BEEP BEEP!")
+    }
+    performMaintenance() {
+      setTimeout(function() {
+        console.log("maintenance complete")
+      }, 3000);
+    }
+  }
+  
+  
+var mySweetRide = new Car('Pontiac', 'Fiero', 1988)
+
+  mySweetRide.honk()
+  mySweetRide.performMaintenance()
